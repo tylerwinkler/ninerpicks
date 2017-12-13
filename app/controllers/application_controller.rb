257aca1d8dc2148865e7023f511a26c49c32a579
Tyler Winkler <twinkle3@uncc.edu>
2017-12-13 17:35:17 +0000

    Test database fixed. Dev database emptied again. test_mode set to false.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
   
-  @@test_mode = true;
+  @@test_mode = false;
   
   def current_user
     if (@@test_mode)
